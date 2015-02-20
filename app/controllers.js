6906e576fd62aa3993acfb8e5948825f70418bd9
unknown <djue129@gmail.com>
2015-02-20 14:08:55 -0800

    add and remove favorites functionality added

--- app/controllers.js
+++ app/controllers.js
@@ -3,12 +3,37 @@
  */
 var foodieAppControllers = angular.module("foodieAppControllers", ["ngRoute","firebase", "foodieAppServices"]);
 
-foodieAppControllers.controller("HomeController", ["$scope", "$firebase", "FirebaseGet", function($scope, firebase, FirebaseGet){
-
-
+foodieAppControllers.controller("HomeController", ["$scope", "$firebase", "FirebaseGet", function($scope, $firebase, FirebaseGet){
     $scope.sandwichList = FirebaseGet.pullFromFirebase();
     console.log($scope.sandwichList);
 
+    $scope.addFavorite = function(item){
+        var ref = new Firebase("https://sandwiches-data.firebaseio.com/" + item.$id);
+        var sync = $firebase(ref);
 
+        sync.$update({addFavorite: 1}).then(function(ref) {
+            console.log("SUCCESS: "+ ref.key());    // bar
+        }, function(error) {
+            console.log("Error: "+ error);
+        });
+    };
+
+    $scope.removeFavorite = function(item){
+        var ref = new Firebase("https://sandwiches-data.firebaseio.com/" + item.$id);
+        var sync = $firebase(ref);
+
+        sync.$update({addFavorite: 0}).then(function(ref) {
+            console.log("SUCCESS: "+ ref.key());    // bar
+        }, function(error) {
+            console.log("Error: "+ error);
+        });
+    };
+
+    //$scope.pullFavorites = function(item){
+    //    var ref = new Firebase("https://sandwiches-data.firebaseio.com/");
+    //    ref.orderByChild("addFavorite").equalTo(1).on("value", function(snapshot){
+    //        return
+    //    })
+    //}
 }]);
 
