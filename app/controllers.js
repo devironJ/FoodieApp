fe2a72a6f5080c3437f2ad80d6dbbeed33aa2dd2
unknown <djue129@gmail.com>
2015-02-20 16:21:03 -0800

    remove/add to favorites updates the populated list

--- app/controllers.js
+++ app/controllers.js
@@ -4,7 +4,10 @@
 var foodieAppControllers = angular.module("foodieAppControllers", ["ngRoute","firebase", "foodieAppServices"]);
 
 foodieAppControllers.controller("HomeController", ["$scope", "$firebase", "FirebaseGet", function($scope, $firebase, FirebaseGet){
-    $scope.sandwichList = FirebaseGet.pullFromFirebase();
+
+    //uses a service to get data from Firebase
+    //$scope.sandwichList = FirebaseGet.pullFromFirebase();
+    $scope.sandwichList = FirebaseGet.pullNonFavorites();
     console.log($scope.sandwichList);
 
     $scope.addFavorite = function(item){
@@ -18,6 +21,27 @@ foodieAppControllers.controller("HomeController", ["$scope", "$firebase", "Fireb
         });
     };
 
+    //$scope.removeFavorite = function(item){
+    //    var ref = new Firebase("https://sandwiches-data.firebaseio.com/" + item.$id);
+    //    var sync = $firebase(ref);
+    //
+    //    sync.$update({addFavorite: 0}).then(function(ref) {
+    //        console.log("SUCCESS: "+ ref.key());    // bar
+    //    }, function(error) {
+    //        console.log("Error: "+ error);
+    //    });
+    //};
+
+    //$scope.pullFavorites = function(item){
+    //    var ref = new Firebase("https://sandwiches-data.firebaseio.com/");
+    //    ref.orderByChild("addFavorite").equalTo(1).on("value", function(snapshot){
+    //        return
+    //    })
+    //}
+}]);
+
+foodieAppControllers.controller("RightSidebarController",["$scope", "$firebase", "FirebaseGet", function($scope, $firebase, FirebaseGet){
+    $scope.sandwichList = FirebaseGet.pullFromFirebase();
     $scope.removeFavorite = function(item){
         var ref = new Firebase("https://sandwiches-data.firebaseio.com/" + item.$id);
         var sync = $firebase(ref);
@@ -28,12 +52,5 @@ foodieAppControllers.controller("HomeController", ["$scope", "$firebase", "Fireb
             console.log("Error: "+ error);
         });
     };
-
-    //$scope.pullFavorites = function(item){
-    //    var ref = new Firebase("https://sandwiches-data.firebaseio.com/");
-    //    ref.orderByChild("addFavorite").equalTo(1).on("value", function(snapshot){
-    //        return
-    //    })
-    //}
 }]);
 
